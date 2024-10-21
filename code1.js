3ac68eed816d05196e1520bd61a2f23a9f392144
BretRen <bretren2013@gmail.com>
2024-10-20 21:07:00 -0700

    b

---
+++ code1.js
@@ -0,0 +1,20 @@
+gdjs.UICode = {};
+gdjs.UICode.localVariables = [];
+
+
+gdjs.UICode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.UICode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.UICode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['UICode'] = gdjs.UICode;
--- gdjs-evtsext__panelspritebutton__panelspritebutton.js
+++ gdjs-evtsext__panelspritebutton__panelspritebutton.js
@@ -529,7 +529,7 @@ for (var i = 0, k = 0, l = gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.Pa
 gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.PanelSpriteButton.prototype.doStepPostEventsContext.GDObjectObjects2.length = k;
 if (isConditionTrue_0) {
 isConditionTrue_0 = false;
-{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(14112628);
+{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(19341860);
 }
 }
 if (isConditionTrue_0) {
@@ -597,7 +597,7 @@ for (var i = 0, k = 0, l = gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.Pa
 gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.PanelSpriteButton.prototype.doStepPostEventsContext.GDObjectObjects2.length = k;
 if (isConditionTrue_0) {
 isConditionTrue_0 = false;
-{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(14116212);
+{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(19345444);
 }
 }
 if (isConditionTrue_0) {
@@ -645,7 +645,7 @@ for (var i = 0, k = 0, l = gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.Pa
 gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.PanelSpriteButton.prototype.doStepPostEventsContext.GDObjectObjects2.length = k;
 if (isConditionTrue_0) {
 isConditionTrue_0 = false;
-{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(14117508);
+{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(19346740);
 }
 }
 if (isConditionTrue_0) {
@@ -687,7 +687,7 @@ for (var i = 0, k = 0, l = gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.Pa
 gdjs.evtsExt__PanelSpriteButton__PanelSpriteButton.PanelSpriteButton.prototype.doStepPostEventsContext.GDObjectObjects1.length = k;
 if (isConditionTrue_0) {
 isConditionTrue_0 = false;
-{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(14118972);
+{isConditionTrue_0 = eventsFunctionContext.getOnceTriggers().triggerOnce(19348204);
 }
 }
 if (isConditionTrue_0) {
